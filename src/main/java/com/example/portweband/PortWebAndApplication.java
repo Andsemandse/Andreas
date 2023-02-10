40abf759ca453bb8e0a123bff55896fc0bbeb035
Andsemandse <andr90m3@stud.kea.dk>
2023-02-10 19:38:51 +0100

    hej

---
+++ src/main/java/com/example/portweband/PortWebAndApplication.java
@@ -0,0 +1,13 @@
+package com.example.portweband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortWebAndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortWebAndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portweband/PortWebAndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portweband;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortWebAndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
